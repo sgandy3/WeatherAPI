1fed24ea0fa5cba0e09c3adcdc9278dfb53e25c9
Your Name <you@example.com>
2015-11-24 20:17:16 -0500

    WeatherAPI

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: WeatherJs
+        public ActionResult WeatherJs()
+        {
+            return View();
+        }
+        // GET: Json GetWeather
+        public JsonResult GetWeather()
+        {
+            Weather weath = new Weather();
+            return Json(weath.getWeatherForcast(),JsonRequestBehavior.AllowGet); //problem??
+        }
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+    }
+}
---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string appid = "4cfcbf5314476ecc9effbaee2bd86060";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID" + appid;
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+           var jsonContent = serializer.Deserialize<object>(content);
+            return jsonContent;
+        }
+    }
+}
